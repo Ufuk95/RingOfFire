2975bd2e4445796a223996677d6c7c8d5409cbda
Ufuk Özsahin <oezsahin95@icloud.com>
2024-05-30 14:17:31 +0200

    second commit

---
+++ src/app/start-screen/start-screen.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-screen',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './start-screen.component.html',
+  styleUrl: './start-screen.component.scss'
+})
+export class StartScreenComponent {
+
+}
